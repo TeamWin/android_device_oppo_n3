4941b8231738ac30953cb7a9ac6b2510d6a0eb4e
Jenkins <bigbiff@teamw.in>
2020-06-18 00:13:31 +0000

    n3: add ext4 to board and remove gps dependency

--- device_n3.mk
+++ device_n3.mk
@@ -1,8 +1,5 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
-# The gps config appropriate for this device
-$(call inherit-product, device/common/gps/gps_us_supl.mk)
-
 $(call inherit-product-if-exists, vendor/oppo/n3/n3-vendor.mk)
 
 DEVICE_PACKAGE_OVERLAYS += device/oppo/n3/overlay
